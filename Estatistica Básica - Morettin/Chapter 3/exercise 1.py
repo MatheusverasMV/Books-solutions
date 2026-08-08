ff5e0180e4c614b1fdd5415cfd9f8f2d5af0ae57
MatheusverasMV <matheusveras051201@gmail.com>
2026-08-07 21:44:36 -0300

    Update exercise 1.py

--- Estatistica Básica - Morettin/Chapter 3/exercise 1.py
+++ Estatistica Básica - Morettin/Chapter 3/exercise 1.py
@@ -13,7 +13,7 @@ def mean(data, frequency):
         initial += 1
     return total/sum(frequency)
 
-def median(data, frequency):
+def median(data):
     if len(data) % 2 == 0:
         median1 = data[len(data)//2 - 1]
         median2 = data[len(data)//2]
@@ -21,8 +21,20 @@ def median(data, frequency):
     else:
         return data[len(data)//2]
 
-def standard_deviation
+def variance(data, frequency):
+    xi = mean(data, frequency)
+    initial = 0
+    total = 0
+    while initial < len(data):
+        total += frequency[initial] * (data[initial] - xi) ** 2
+        initial += 1
+    return total / sum(frequency)
+
+def standard_deviation(data, frequency):
+    return variance(data, frequency) ** 0.5
 
 
 print(f"Mean: {mean(list(books_error_data.keys()), list(books_error_data.values()))}")
-print(f"Median: {median(list(books_error_data.keys()), list(books_error_data.values()))}")
+print(f"Median: {median(list(books_error_data.keys()))}")
+print(f"Variance: {variance(list(books_error_data.keys()), list(books_error_data.values()))}")
+print(f"Standard Deviation: {standard_deviation(list(books_error_data.keys()), list(books_error_data.values()))}")
