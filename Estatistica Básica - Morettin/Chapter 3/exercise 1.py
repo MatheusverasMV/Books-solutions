42cbcf0bb88d4674e210937cfb6e6b3655087a83
MatheusverasMV <matheusveras051201@gmail.com>
2026-08-07 21:35:57 -0300

    Update exercise 1.py

--- Estatistica Básica - Morettin/Chapter 3/exercise 1.py
+++ Estatistica Básica - Morettin/Chapter 3/exercise 1.py
@@ -1,3 +1,10 @@
+books_error_data = { 0:25,
+                    1:20,
+                    2:3,
+                    3:1,
+                    4:1
+} 
+
 def mean(data, frequency):
     initial = 1
     total = data[0] * frequency[0]
@@ -6,18 +13,14 @@ def mean(data, frequency):
         initial += 1
     return total/sum(frequency)
 
-books_error_data = { 0:25,
-                    1:20,
-                    2:3,
-                    3:1,
-                    4:1
-} 
-
-def median
-
-
-
-
+def median(data, frequency):
+    if len(data) % 2 == 0:
+        median1 = data[len(data)//2 - 1]
+        median2 = data[len(data)//2]
+        return (median1 + median2) / 2
+    else:
+        return data[len(data)//2]
 
 
 print(f"Mean: {mean(list(books_error_data.keys()), list(books_error_data.values()))}")
+print(f"Median: {median(list(books_error_data.keys()), list(books_error_data.values()))}")
